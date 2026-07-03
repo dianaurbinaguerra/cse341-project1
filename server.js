7d442c1b8034f996f5e10f73e674064c728fd335
Dianauguerra <diana.1609@hotmail.com>
2026-07-03 13:07:02 -0600

    Connect your node project to MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
+
+const mongodb = require('./data/database')
 const app = express();
 
 const port = process.env.PORT || 8080;
 
 app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        app.listen(port, () => {
+            console.log("Connected to the database");
+        });
+    }
+})
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
